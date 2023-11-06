5d071be24d84a7ed88606b508295269f18f9c1a8
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2023-11-06 10:41:16 +0700

    Update README.md
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

--- README.md
+++ README.md
@@ -8,11 +8,14 @@ Building the escript:
 
 ```shell
 $ mix escript.build
+$ mix escript.install
 ```
 
 The `testrail_format` binary will be presented. 
 
 ## Usage
+
+### Formatting the file
 
 The command accept the filename as a first argument:
 
@@ -28,3 +31,18 @@ $ testrail_format test.tbl
 
 To edit inplace, add `-w` (or `--write`) during call the command.
 
+### Using standard input (stdin)
+
+This program can also support formatting code via standard input. The benefit is that 
+it can integrate with the editors that pass a buffer into a program via this channel. To 
+use it, add `-i` option during call a program:
+
+```shell
+$ cat test.tbl
+|| a |b|c
+||d| e   |f
+$ testrail_format -i < test.tbl
+|| a | b | c
+|| d | e | f
+
+```
--- lib/testrail_table_formatter/cli.ex
+++ lib/testrail_table_formatter/cli.ex
@@ -1,24 +1,39 @@
 defmodule TestrailTableFormatter.CLI do
   @options [
-    write: :boolean
+    write: :boolean,
+    read_from_stdin: :boolean
   ]
 
   @aliases [
-    w: :write
+    w: :write,
+    i: :read_from_stdin
   ]
 
   def main(args) do
-    {options, [path], _} = OptionParser.parse(args, aliases: @aliases, strict: @options)
+    {options, argv, _} = OptionParser.parse(args, aliases: @aliases, strict: @options)
 
+    from_stdin? = Keyword.get(options, :read_from_stdin, false)
     write? = Keyword.get(options, :write, false)
 
-    content = File.read!(path)
-    formatted = TestrailTableFormatter.format(content)
+    cond do
+      write? and from_stdin? ->
+        raise "ERROR: Cannot specify `--write` and `--read_from_stdin` at the same time."
 
-    if write? do
-      File.write!(path, formatted)
-    else
-      IO.puts(formatted)
+      from_stdin? ->
+        IO.read(:stdio, :all) |> format() |> IO.puts()
+
+      true ->
+        [path] = argv
+        content = File.read!(path)
+        formatted = format(content)
+
+        if write? do
+          File.write!(path, formatted)
+        else
+          IO.puts(formatted)
+        end
     end
   end
+
+  defdelegate format(content), to: TestrailTableFormatter
 end
